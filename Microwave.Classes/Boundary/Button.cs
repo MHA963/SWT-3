84c3dbd3ee6d5f2f94d32164d9fb745159c18a03
JustOnlyTim <madshassestavnsbo@googlemail.com>
2023-04-26 13:17:12 +0200

    Modified Tests (added one), and added comments to the ones affected by the beep, for clarification/why tests fail.

--- Microwave.Classes/Boundary/Button.cs
+++ Microwave.Classes/Boundary/Button.cs
@@ -16,7 +16,7 @@ namespace Microwave.Classes.Boundary
         public void Press()
         {
             Pressed?.Invoke(this, EventArgs.Empty);
-            Console.Beep(2500, 750);
+            Console.Beep(2500, 750); // causes error in the two button tests, due to pipeline not supporting beep(). 
         }
     }
 }
