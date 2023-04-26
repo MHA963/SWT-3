90674d1035b44fdb2856d3e00f7cda332546f7af
MHA <201910895@post.au.dk>
2023-04-26 19:08:05 +0200

    Update ITimer.cs

--- Microwave.Classes/Interfaces/ITimer.cs
+++ Microwave.Classes/Interfaces/ITimer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ namespace Microwave.Classes.Interfaces
 {
     public interface ITimer
     {
-        int TimeRemaining { get; }
+        int TimeRemaining { get; set;}
         event EventHandler Expired;
         event EventHandler TimerTick;
 
